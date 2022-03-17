32d42272f77c1b6b97d950c27528bac41780f0e0
TheJongu <jongu.gamer@gmx.de>
2022-03-17 11:35:18 +0100

    Extended Parser, Added basic tests
    Files can now be read and parsed
    Debugging Output has been added for simple debugging

--- src/main/java/de/jgu/parser/CSVParser.java
+++ src/main/java/de/jgu/parser/CSVParser.java
@@ -1,30 +1,101 @@
 package de.jgu.parser;
 
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvException;
 
 public class CSVParser {
 	
+	
 
 	CSVReader CSVReader;
+	// Named columns
+	private List<String> headLine;
+	// Values in the CSV
+	// List[ ] = row in the file
+	// String[index] = column of the row
+	private List<String[]> values;
 
-	public CSVParser(String aCSVFilePath) throws FileNotFoundException, IOException, CsvException {
-		// Convert the given FilePath to a File		
+	public CSVParser(){
+		// Create empty Parser
 		System.out.println("Parser has been created");
-		try (CSVReader reader = new CSVReader(new FileReader(aCSVFilePath))) {
-		      List<String[]> r = reader.readAll();
-		      r.forEach(x -> System.out.println(Arrays.toString(x)));
-		  }
-				
 	}
 	
+	public boolean parseFile(String aCSVFilePath) {
+		// new File is parsed, remove old information
+		headLine = null;
+		values = null;
+		CSVReader = null;
+		
+		try (CSVReader reader = new CSVReader(new FileReader(aCSVFilePath))) {
+			List<String[]> csvRowList = reader.readAll();
+		    
+			Iterator<String[]> csvIterator = csvRowList.iterator();
+			while(csvIterator.hasNext()) {
+				String[] nextRow = csvIterator.next();
+				// headline
+				// first time running: Headline == null -> Set Headline
+				if(headLine == null) {
+					headLine = new ArrayList<String>();
+					for(int i = 0; i < nextRow.length; i++) {
+						headLine.add(nextRow[i]);
+					}
+					continue;
+				}
+				// everytime except first time -> Add value of CSV to values list
+				// Body of Csv
+				if(values == null) {
+					values = new ArrayList<String[]>();
+				}
+				values.add(nextRow);
+			}
+		    // file has been parsed successfully
+		    return true;
+		    
+		}catch(Exception e) {
+			// A full and correct logging concept should be used
+			System.out.print("An Exception occured" + e.toString());
+			// Something went wrong, tell the user the file has not been parsed
+			return false;
+		}
+	}
 	
+	/**
+	 * Print the parsed File
+	 */
+	public void printParsedFile() {
+		if(headLine == null) {
+			System.out.println("No File Parsed");
+			return;
+		}
+		
+		// Print headline
+		this.headLine.stream().forEach(s -> System.out.print(s.toString() + ", "));
+		// print values in Rows
+		this.values.stream().forEach(s -> {
+			String outputString = "";
+			for(int i = 0; i<s.length; i++) {
+				outputString += s[i] + ", "; 
+			}
+			System.out.println(outputString);
+		});
+	}
+	
+	// generated getter
+	public List<String> getHeadLine() {
+		return headLine;
+	}
+
+	public List<String[]> getValues() {
+		return values;
+	}
 	
 }
--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -3,7 +3,11 @@ package de.exxcellent.challenge;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import de.jgu.parser.CSVParser;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Example JUnit 5 test case.
@@ -26,6 +30,28 @@ class AppTest {
     @Test
     void runFootball() {
         App.main("--football", "football.csv");
+    }
+    
+
+    @Test
+    void footballFileExists() {
+    	// A file with name football.csv in resources dir is available
+   	 	assertTrue(App.getWeatherPath().endsWith("src\\main\\resources\\de\\exxcellent\\challenge\\football.csv"));
+    }
+
+    @Test
+    void weaterFileExists() {
+    	// A file with name weather.csv in resources dir is available 
+    	assertTrue(App.getWeatherPath().endsWith("src\\main\\resources\\de\\exxcellent\\challenge\\weather.csv"));
+    }
+    
+    @Test
+    void parsingTest() {
+    	CSVParser theParser = new CSVParser();
+    	
+    	// File has been parsed successfully
+    	assertTrue(theParser.parseFile(App.getFootballPath()));
+    	assertTrue(theParser.parseFile(App.getWeatherPath()));    	
     }
 
 }
